0d9f048f39d4c2fddc932c9c2e14f37b6ac3d8c2
gongxh <gongxinhai@kunpo.cc>
2025-06-04 23:10:59 +0800

    init

---
+++ rollup.config.mjs
@@ -0,0 +1,86 @@
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+
+export default [
+    {
+        // 生成未压缩的 JS 文件
+        input: 'src/kunpocc-behaviortree.ts',
+        external: ['cc', 'fairygui-cc'],
+        output: [
+            {
+                file: 'dist/kunpocc-behaviortree.mjs',
+                format: 'esm',
+                name: 'kunpocc-behaviortree'
+            },
+            {
+                file: 'dist/kunpocc-behaviortree.cjs',
+                format: 'cjs',
+                name: 'kunpocc-behaviortree'
+            }
+        ],
+        plugins: [
+            typescript({
+                tsconfig: './tsconfig.json',
+                importHelpers: false,
+                compilerOptions: {
+                    target: "es6",
+                    module: "es6",
+                    experimentalDecorators: true, // 启用ES装饰器。
+                    strict: true,
+                    strictNullChecks: false,
+                    moduleResolution: "Node",
+                    skipLibCheck: true,
+                    esModuleInterop: true,
+                }
+            })
+        ]
+    },
+    {
+        // 生成压缩的 JS 文件
+        input: 'src/kunpocc-behaviortree.ts',
+        external: ['cc', 'fairygui-cc'],
+        output: [
+            {
+                file: 'dist/kunpocc-behaviortree.min.mjs',
+                format: 'esm',
+                name: 'kunpocc-behaviortree'
+            },
+            {
+                file: 'dist/kunpocc-behaviortree.min.cjs',
+                format: 'cjs',
+                name: 'kunpocc-behaviortree'
+            }
+        ],
+        plugins: [
+            typescript({
+                tsconfig: './tsconfig.json',
+                importHelpers: false,
+                compilerOptions: {
+                    target: "es6",
+                    module: "es6",
+                    experimentalDecorators: true, // 启用ES装饰器。
+                    strict: true,
+                    strictNullChecks: false,
+                    moduleResolution: "Node",
+                    skipLibCheck: true,
+                    esModuleInterop: true,
+                }
+            }),
+            terser()
+        ]
+    },
+    {
+        // 生成声明文件的配置
+        input: 'src/kunpocc-behaviortree.ts',
+        output: {
+            file: 'dist/kunpocc-behaviortree.d.ts',
+            format: 'es'
+        },
+        plugins: [dts({
+            compilerOptions: {
+                stripInternal: true
+            }
+        })]
+    }
+]; 
---
+++ src/kunpocc-behaviortree.ts
@@ -0,0 +1,14 @@
+
+/** 行为树 */
+export { Agent as Agent } from "./behaviortree/Agent";
+export { BehaviorTree } from "./behaviortree/BehaviorTree";
+export { Blackboard } from "./behaviortree/Blackboard";
+export * as Action from "./behaviortree/BTNode/Action";
+export { BaseNode as Node } from "./behaviortree/BTNode/BaseNode";
+export * as Composite from "./behaviortree/BTNode/Composite";
+
+export { Condition } from "./behaviortree/BTNode/Condition";
+export * as Decorator from "./behaviortree/BTNode/Decorator";
+export { Status } from "./behaviortree/header";
+export { Ticker } from "./behaviortree/Ticker";
+
